b610a252d8e024796b4db96e66bc8932ef71ebe0
Visharad <visharadborsutkar777@gmail.com>
2022-07-14 04:59:02 +0530

    Init

---
+++ index.js
@@ -0,0 +1,60 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import Note from './model/Note.js';
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 4001;
+const CONNECTION_URL = "mongodb+srv://visharad:0Bnk5uamhybQQUap@cluster0.p5d50.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true}, () =>{
+    console.log("DB connected");
+});
+
+app.get('/api/getAll', (req, res) => {
+    Note.find({}, (err, noteList) => {
+        if(err) console.log(err);
+        else{
+            res.status(200).send(noteList);
+        }
+    });
+});
+
+app.post('/api/addNew', (req, res) => {
+    const { title, content } = req.body;
+    const noteObj = new Note({
+        title : title,
+        content : content
+    });
+    noteObj.save(err => {
+        if(err) console.log(err);
+        else console.log("Successfully added");
+
+        Note.find({}, (err, noteList) => {
+            if(err) console.log(err);
+            else{
+                res.status(200).send(noteList);
+            }
+        });
+    });
+});
+
+app.post('/api/delete', (req, res) => {
+    const { id } = req.body;
+    Note.deleteOne({_id: id}, () => {
+        Note.find({}, (err, noteList) => {
+            if(err) console.log(err);
+            else{
+                res.status(200).send(noteList);
+            }
+        }); 
+    });
+});
+
+app.listen(PORT, () => {
+    console.log("backend connected at " + PORT)
+});

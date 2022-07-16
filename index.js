2c4326feecf05929652d78576fde9e91cb456c1c
Visharad <visharadborsutkar777@gmail.com>
2022-07-16 19:38:41 +0530

    heroku procfile

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: tru
 
 import Note from './model/Note.js'; 
 
-app.get("", (req, res) => {
-    res.send("Server is running");
+app.get("", async (req, res) => {
+    await res.send("Server is running");
 });
 
 // fetching all the notes/data
-app.get('/api/getAll', (req, res) => {
-    Note.find({}, (err, noteList) => {
+app.get('/api/getAll', async (req, res) => {
+    await Note.find({}, (err, noteList) => {
         if(err) console.log(err);
         else{
             res.status(200).send(noteList);
@@ -31,13 +31,13 @@ app.get('/api/getAll', (req, res) => {
 });
 
 // adding new note
-app.post('/api/addNew', (req, res) => {
+app.post('/api/addNew', async (req, res) => {
     const { title, content } = req.body;
     const noteObj = new Note({
         title : title,
         content : content
     });
-    noteObj.save(err => {
+    await noteObj.save(err => {
         if(err) console.log(err);
         else console.log("Successfully added");
 
@@ -51,9 +51,9 @@ app.post('/api/addNew', (req, res) => {
 });
 
 // deleteing all the nodes
-app.post('/api/delete', (req, res) => {
+app.post('/api/delete', async (req, res) => {
     const { id } = req.body;
-    Note.deleteOne({_id: id}, () => {
+    await Note.deleteOne({_id: id}, () => {
         Note.find({}, (err, noteList) => {
             if(err) console.log(err);
             else{
